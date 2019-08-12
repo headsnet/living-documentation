cccdbed1e19ab79925b09061eca3d7c9a3c72bf9
Ben Roberts <ben@headsnet.com>
2019-08-12 18:30:38 +0200

    Configure as Symfony bundle

---
+++ src/HeadsnetLivingDocumentationBundle.php
@@ -0,0 +1,14 @@
+<?php
+namespace Headsnet\LivingDocumentation;
+
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle configuration
+ */
+class HeadsnetLivingDocumentationBundle extends Bundle
+{
+
+}
