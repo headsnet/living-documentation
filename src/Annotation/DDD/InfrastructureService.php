0db4c06a5e25919308848f8073ded388d31559e6
Ben Roberts <ben@headsnet.com>
2019-08-20 09:09:39 +0200

    New annotation for infrastructure services

---
+++ src/Annotation/DDD/InfrastructureService.php
@@ -0,0 +1,17 @@
+<?php
+namespace Headsnet\LivingDocumentation\Annotation\DDD;
+
+use Doctrine\Common\Annotations\Annotation;
+use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
+
+/**
+ * An infrastructure service lives in the infrastructure layer
+ * and may be called by services in the application layer
+ *
+ * @Annotation
+ * @Target({"CLASS"})
+ */
+final class InfrastructureService implements LivingDocumentationAnnotation
+{
+
+}
