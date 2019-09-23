f098933c673df8b237a36e9aad50beb586e42336
Ben Roberts <ben@headsnet.com>
2019-09-23 19:06:22 +0200

    Add GuidedTour annotation

---
+++ src/Annotation/CuratedContent/GuidedTour.php
@@ -0,0 +1,70 @@
+<?php
+namespace Headsnet\LivingDocumentation\Annotation\DDD;
+
+use Doctrine\Common\Annotations\Annotation;
+use Headsnet\LivingDocumentation\Annotation\BaseAnnotation;
+use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
+
+/**
+ * A Guided Tour annotation is used in multiple places to describe
+ * a sequence of processing. For example, several Guided Tour annotations
+ * can be used to lead the reader through the process of receiving a
+ * request, processing data, dispatching events and returning a response etc.
+ *
+ * @Annotation
+ * @Target({"CLASS", "METHOD"})
+ */
+final class GuidedTour extends BaseAnnotation implements LivingDocumentationAnnotation
+{
+    /**
+     * @var string
+     * @Required
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @Required
+     */
+    private $description;
+
+    /**
+     * @var int
+     * @Required
+     */
+    private $rank;
+
+    /**
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        $this->name = $values['name'];
+        $this->description = $values['description'];
+        $this->rank = $values['rank'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->convertMultiLine($this->name);
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->convertMultiLine($this->description);
+    }
+
+    /**
+     * @return int
+     */
+    public function getRank(): int
+    {
+        return $this->rank;
+    }
+}
