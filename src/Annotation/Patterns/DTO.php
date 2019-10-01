b7be737de74c5fdcea22cdb7012a0832f206662a
Ben Roberts <ben@headsnet.com>
2019-10-01 17:38:38 +0200

    Add idempotent property to DTO trait

--- src/Annotation/Patterns/DTO.php
+++ src/Annotation/Patterns/DTO.php
@@ -3,6 +3,7 @@ namespace Headsnet\LivingDocumentation\Annotation\Patterns;
 
 use Doctrine\Common\Annotations\Annotation;
 use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
+use Headsnet\LivingDocumentation\Annotation\Traits\IdempotentTrait;
 
 /**
  * A DTO class is a simple object containing primitive properties
@@ -15,5 +16,13 @@ use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
  */
 final class DTO implements LivingDocumentationAnnotation
 {
+    use IdempotentTrait;
 
+    /**
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        $this->idempotent = $values['idempotent'] ?? '';
+    }
 }
