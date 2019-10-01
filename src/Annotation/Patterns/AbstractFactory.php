d297825296b48c59375bff306eeee675ffae8b50
Ben Roberts <ben@headsnet.com>
2019-10-01 17:38:05 +0200

    Add AbstractFactory annotation

---
+++ src/Annotation/Patterns/AbstractFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Headsnet\LivingDocumentation\Annotation\Patterns;
+
+use Doctrine\Common\Annotations\Annotation;
+use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
+
+/**
+ * The abstract factory pattern provides a way to encapsulate a group of individual
+ * factories that have a common theme without specifying their concrete classes.
+ *
+ * @see https://en.wikipedia.org/wiki/Abstract_factory_pattern
+ *
+ * @Annotation
+ * @Target({"CLASS"})
+ */
+final class AbstractFactory implements LivingDocumentationAnnotation
+{
+
+}
