4d4331ecf6945f70f45a26b48839ef044bf412c6
Ben Roberts <ben@headsnet.com>
2019-10-30 10:48:13 +0100

    Add specific DTO annotations

---
+++ src/Annotation/Patterns/FormDTO.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Headsnet\LivingDocumentation\Annotation\Patterns;
+
+use Headsnet\LivingDocumentation\Annotation\Traits\ImmutableTrait;
+
+/**
+ * A DTO class is a simple object containing primitive properties
+ * used to transfer data between applications or application layers.
+ *
+ * Form DTO's are used as the data object for web forms, and avoid
+ * coupling e.g. entities from the domain layer with forms which
+ * live in the user interface layer.
+ *
+ * @see https://en.wikipedia.org/wiki/Data_transfer_object
+ * @see https://stovepipe.systems/post/avoiding-entities-in-forms
+ *
+ * @Annotation
+ * @Target({"CLASS"})
+ */
+final class FormDTO
+{
+    use ImmutableTrait;
+
+    public function __construct(array $values)
+    {
+        $this->immutable = $values['immutable'] ?? '';
+    }
+}
