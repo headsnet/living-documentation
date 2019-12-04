3b3f908023f987738598bd22977c79c296cc81d5
Ben Roberts <ben@headsnet.com>
2019-12-04 14:05:34 +0100

    Add refactoring annotations

---
+++ src/Annotation/Refactoring/AwaitingRefactoring.php
@@ -0,0 +1,54 @@
+<?php
+namespace Headsnet\LivingDocumentation\Annotation\DDD;
+
+use Doctrine\Common\Annotations\Annotation;
+use Headsnet\LivingDocumentation\Annotation\BaseAnnotation;
+use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
+
+/**
+ * To keep track of items to refactor when planning an application-wide change.
+ *
+ * When many components in a system have to be refactored towards a new paradigm,
+ * instead of attempting to perform these all at once, it is often better to keep
+ * the application in a deployable state whilst picking off the refactorings one
+ * at a time.
+ *
+ * This annotation can be used to label specific planned refactorings. It is then
+ * searchable to provide a list of refactoring tasks not yet undertaken. Eventually
+ * as all the refactorings are completed, the annotation will disappear from the
+ * codebase.
+ *
+ * TIP: Include a link to your Issue Tracker in the description
+ *
+ * @Annotation
+ * @Target({"CLASS", "METHOD", "PROPERTY"})
+ */
+final class AwaitingRefactoring extends BaseAnnotation implements LivingDocumentationAnnotation
+{
+    /**
+     * @var string
+     * @Required
+     */
+    private $summary;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    public function __construct(array $values)
+    {
+        $this->summary = $values['summary'];
+        $this->description = $values['description'] ?? '';
+    }
+
+    public function getSummary(): string
+    {
+        return $this->convertMultiLine($this->summary);
+    }
+
+    public function getDescription(): string
+    {
+        return $this->convertMultiLine($this->description);
+    }
+}
---
+++ src/Annotation/Refactoring/WorkInProgress.php
@@ -0,0 +1,53 @@
+<?php
+namespace Headsnet\LivingDocumentation\Annotation\DDD;
+
+use Doctrine\Common\Annotations\Annotation;
+use Headsnet\LivingDocumentation\Annotation\BaseAnnotation;
+use Headsnet\LivingDocumentation\Annotation\LivingDocumentationAnnotation;
+
+/**
+ * Labels code that is only part way through a large refactoring.
+ *
+ * Often code goes through a series of small refactorings in order
+ * to achieve a larger refactoring. Sometimes these all have to be
+ * completed in order to be left with functioning code, however it
+ * is often the case that the code is still fully functional before
+ * the larger refactoring is fully completed.
+ *
+ * This annotation will highlight that the code is being worked on
+ * and has not yet reached the intended destination.
+ *
+ * TIP: Include a link to your Issue Tracker in the description
+ *
+ * @Annotation
+ * @Target({"CLASS", "METHOD", "PROPERTY"})
+ */
+final class WorkInProgress extends BaseAnnotation implements LivingDocumentationAnnotation
+{
+    /**
+     * @var string
+     * @Required
+     */
+    private $summary;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    public function __construct(array $values)
+    {
+        $this->summary = $values['summary'];
+        $this->description = $values['description'] ?? '';
+    }
+
+    public function getSummary(): string
+    {
+        return $this->convertMultiLine($this->summary);
+    }
+
+    public function getDescription(): string
+    {
+        return $this->convertMultiLine($this->description);
+    }
+}
